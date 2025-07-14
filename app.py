d6bdab6a0f342c82d12ea114f88dfd7287fd0c63
NeuralNexusLab <neuralnexuslab@hotmail.com>
2025-07-14 08:49:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,17 @@ def home():
 def hi (user):
     return "hello! " + user
 
+@app.route("/r/nh")
+def nh ():
+    return "<script>window.location.href='https://nethacker.onrender.com'</script>"
+
+@app.route("/r/snh")
+def snh ():
+    return "<script>window.location.href='https://nethacker-nh.onrender.com'</script>"
+
+@app.route("/r/gnh")
+def gnh ():
+    return "<script>window.location.href='https://neuralnexuslab-nh.github.io'</script>"
+
 if __name__ == "__main__":
     app.run(debug=True)
